a2a843bda63733714d92ba700e79ce2246b13497
marcelo <mdsv14@gmail.com>
2025-04-29 02:10:37 -0500

    refactor: update clean_positions script to use dynamic script directory

--- scripts/clean_positions.sh
+++ scripts/clean_positions.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-psql -U postgres -d locateme -c "\i /var/www/locateme/locateme-api/scripts/clean_positions.sql"
+
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Execute the SQL script using psql
+psql -h localhost -U postgres -d locateme -c "\i ${SCRIPT_DIR}/clean_positions.sql"
